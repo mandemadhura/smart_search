074666470ba240c15c996d6909afaaa5b0b1906b
Naveena Kondraganti <naveena.kondraganti@gwu.edu>
2025-08-10 15:02:35 -0500

    Loading the env variables to config

--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,11 @@
 
-endpoint = "https://proxvision-app-ai.openai.azure.com/openai/deployments/gpt-4o"
-api_key = ""
-model_name = "gpt-4o"
+import os
+from dotenv import load_dotenv
+
+# Load .env file into environment variables
+load_dotenv()
+
+# Access variables from environment
+endpoint = os.getenv("ENDPOINT")
+api_key = os.getenv("API_KEY")
+model_name = os.getenv("MODEL_NAME")
